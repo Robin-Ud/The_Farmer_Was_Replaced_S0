f75798a4e5f7f444c5d9ad040561767fab4e116c
RobinUd <matheusprestes2006@gmail.com>
2026-02-04 02:01:08 -0300

    Minor fixes

--- culturas_base.py
+++ culturas_base.py
@@ -1,20 +1,20 @@
-from __builtins__ import *
+from game_defs import *
 import mover
 import basic
 
 def main_trigo():
-    basic.verificar_solo(Grounds.Grassland)
+	basic.verificar_solo(Grounds.Grassland)
 
 def main_madeira():
-    main_trigo()
-    if (get_pos_x() % 2 == 0) and (get_pos_y() % 2 == 0):
-        plant(Entities.Tree)
-    else:
-        plant(Entities.Bush)
+	main_trigo()
+	if (get_pos_x() % 2 == 0) and (get_pos_y() % 2 == 0):
+		plant(Entities.Tree)
+	else:
+		plant(Entities.Bush)
 
 def main_cenoura():
-    basic.verificar_solo(Grounds.Soil)
-    plant(Entities.Carrot)
+	basic.verificar_solo(Grounds.Soil)
+	plant(Entities.Carrot)
 
 # def farmar_basicos(cultura, meta):
 #     # Mapeamento direto: associa a função ao item
@@ -37,20 +37,20 @@ def main_cenoura():
 #         quick_print("Erro: Cultura nao existe na tabela!")
 
 def farmar_basicos(cultura, meta):
-    # Valores padrão (caso digite errado, farma trigo)
-    item = Items.Hay
-    funcao = main_trigo
+	# Valores padrão (caso digite errado, farma trigo)
+	item = Items.Hay
+	funcao = main_trigo
 
-    if cultura == "trigo":
-        item = Items.Hay
-        funcao = main_trigo
-    elif cultura == "madeira":
-        item = Items.Wood
-        funcao = main_madeira
-    elif cultura == "cenoura":
-        item = Items.Carrot
-        funcao = main_cenoura
-        
-    # Loop de execução
-    while num_items(item) < meta:
-        mover.percorrer_campo(funcao)
+	if cultura == "trigo":
+		item = Items.Hay
+		funcao = main_trigo
+	elif cultura == "madeira":
+		item = Items.Wood
+		funcao = main_madeira
+	elif cultura == "cenoura":
+		item = Items.Carrot
+		funcao = main_cenoura
+		
+	# Loop de execução
+	while num_items(item) < meta:
+		mover.percorrer_campo(funcao)
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import culturas_base 
-from __builtins__ import *
+from game_defs import *
 
 meta = 1
 while True:
-    meta *= 1.1
+	meta *= 1.1
+	quick_print(meta*5, meta*4, meta*3)
+	culturas_base.farmar_basicos("trigo", meta*5)
+	culturas_base.farmar_basicos("madeira", meta*4)
+	culturas_base.farmar_basicos("cenoura", meta*3)
 
-    culturas_base.farmar_basicos("trigo", meta*5)
-    culturas_base.farmar_basicos("madeira", meta*4)
-    culturas_base.farmar_basicos("cenoura", meta*3)
 
